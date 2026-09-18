44041cc33593569aa6f110047e1c56d90859ec81
tsupplis <tsupplis@users.noreply.github.com>
2026-09-18 22:37:46 +0100

    msc51 compiler added

---
+++ examples/hellomsc.c
@@ -0,0 +1,15 @@
+/* Sample ANSI C program */
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main(int argc, char **argv)
+{
+    int res;
+    res = add(10, 20);
+    printf("Hello from c (msc): add(10,20)=%d\n", res);
+    return 0;
+}
