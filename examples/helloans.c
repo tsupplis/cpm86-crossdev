594be4fa2d83b884628ca25bbfc79ac752733328
tsupplis <tsupplis@users.noreply.github.com>
2026-09-20 22:05:35 +0100

    commend on fp support

--- examples/helloans.c
+++ examples/helloans.c
@@ -10,6 +10,6 @@ int main(int argc, char **argv)
 {
     int res;
     res = add(10, 20);
-    printf("Hello from c (ansi cc): add(10,20)=%d\n", res);
+    printf("Hello from c (ansi cc): add(10,20)=%d", res);
     return 0;
 }
---
+++ examples/hellocfp.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+    float a=3.5;
+    printf("Hello from c ansic with fp): %f",a*a);
+    return 0;
+}
